72df9b3cfddc7e876b49376123e2427c325a2bcb
mohamed-younes16 <bettiouim77@gmail.com>
2023-12-31 08:16:48 +0100

    improved

--- app/api/stores/[storeId]/products/route.ts
+++ app/api/stores/[storeId]/products/route.ts
@@ -9,10 +9,31 @@ export async function GET(
       if (!params.storeId) {
         return new NextResponse("Store id is required", { status: 400 });
       }
+      
+      const { searchParams } = new URL(req.url)
+      const categoryId = searchParams.get('categoryId') || undefined;
+      const colorId = searchParams.get('colorId') || undefined;
+      const sizeId = searchParams.get('sizeId') || undefined;
+      const isFeatured = searchParams.get('isFeatured');
+  
   
       const products = await prismadb.product.findMany({
         where: {
-          storeId: params.storeId
+          storeId: params.storeId,
+          categoryId,
+          colorId,
+          sizeId,
+          isFeatured: isFeatured ? true : undefined,
+          isArchived: false,
+        },
+        include: {
+          images: true,
+          category: true,
+          color: true,
+          size: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
         }
       });
     
