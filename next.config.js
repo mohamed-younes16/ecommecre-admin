1892efb4c33b4a24123ab67c17fbcd59efcbd8d5
mohamed-younes16 <bettiouim77@gmail.com>
2023-12-31 06:35:46 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-experimental:{serverActions:true},
-    images:{
-        unoptimized:true,
-        formats:["image/avif","image/webp"],
-        domains:["swiperjs.com","utfs.io","s3.amazonaws.com","*"],
+  images: {
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+    domains: ["swiperjs.com", "utfs.io", "s3.amazonaws.com", "*"],
+  },
+};
 
-    
-    }, 
-    //  webpack: (
-    //     config,
-    //     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    //   ) => {
-    //     config.module.rules.push({
-    //       test: /\.mjs$/,
-    //       include: /node_modules/,
-    //       type: "javascript/auto",
-    //     });
-    //     return config;
-    //   },
-
-
-}
-
-module.exports = nextConfig
- 
+module.exports = nextConfig;
